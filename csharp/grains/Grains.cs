23e171fc48118075d6918d10fe2f4be2e1b9edcd
CePur <36715887+CePur@users.noreply.github.com>
2020-06-03 00:20:50 +0300

    Add Grains exercise

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Grains
+{
+    private const int totalSquares = 64;
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64) throw new ArgumentOutOfRangeException();
+        return (ulong)Math.Pow(2, n - 1);
+
+    }
+
+    public static ulong Total()
+    {
+        ulong total = 0;
+
+        for (int i = 1; i <= totalSquares; i++)
+        {
+            total += Square(i);
+        }
+
+        return total;
+    }
+}
