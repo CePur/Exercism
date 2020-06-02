7174d73cb3cd0a168dff4fe3213b6b09512d6b87
CePur <36715887+CePur@users.noreply.github.com>
2020-06-03 00:55:44 +0300

    Refactor Grains exercise

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -3,12 +3,7 @@
 public static class Grains
 {
     private const int totalSquares = 64;
-    public static ulong Square(int n)
-    {
-        if (n < 1 || n > 64) throw new ArgumentOutOfRangeException();
-        return (ulong)Math.Pow(2, n - 1);
-
-    }
+    public static ulong Square(int n) => (n < 1 || n > 64) ? throw new ArgumentOutOfRangeException() : (ulong)Math.Pow(2, n - 1);
 
     public static ulong Total()
     {
