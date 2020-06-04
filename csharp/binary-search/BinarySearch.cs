16f402a3231b424cb9ac48511ebbb913b55d781b
CePur <36715887+CePur@users.noreply.github.com>
2020-06-05 02:04:40 +0300

    Add Binary Search exercise

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        int low = 0, high = input.Length - 1, mid;
+
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+
+            if (value < input[mid])
+                high = mid - 1;
+
+            else if (value > input[mid])
+                low = mid + 1;
+            else
+                return mid;
+        }
+
+        return -1;
+    }
+}
