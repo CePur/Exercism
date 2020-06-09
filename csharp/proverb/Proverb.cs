9e5f85c60ee77bae88d814ef4cbb1a3f80ca0eab
CePur <36715887+CePur@users.noreply.github.com>
2020-06-10 00:37:36 +0300

    Add proverb exercise

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        var str = new List<string>();
+
+        if (subjects.Length > 1)
+        {
+            for (int i = 0; i < subjects.Length - 1; i++)
+            {
+                str.Add($"For want of a {subjects[i]} the {subjects[i + 1]} was lost.");
+            }
+        }
+
+        if (subjects.Length != 0)
+        {
+            str.Add($"And all for the want of a {subjects[0]}.");
+        }
+
+        return str.ToArray();
+    }
+}
