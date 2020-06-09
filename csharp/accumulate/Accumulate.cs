4227ed8365c063cffc2b2240d8cb69103921b0d1
CePur <36715887+CePur@users.noreply.github.com>
2020-06-10 00:02:24 +0300

    Add solution for Accumulate exercise

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -5,6 +5,11 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (collection == null || func == null) throw new ArgumentNullException();
+
+        foreach (var element in collection)
+        {
+            yield return func(element);
+        }
     }
 }
