d696ff4d1a7f4c6d93c909009f79554c2f036d61
CePur <cenkpurtas@hotmail.com>
2021-01-14 11:11:07 +0300

    add new exercise

---
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+public static class ProteinTranslation
+{
+    private readonly static Dictionary<string, string> dict = new Dictionary<string, string>()
+        {
+            { "AUG","Methionine" },
+            { "UUU","Phenylalanine" },
+            { "UUC","Phenylalanine" },
+            { "UUA","Leucine" },
+            { "UUG","Leucine" },
+            { "UCU","Serine" },
+            { "UCC","Serine" },
+            { "UCA","Serine" },
+            { "UCG","Serine" },
+            { "UAU","Tyrosine" },
+            { "UAC","Tyrosine" },
+            { "UGU","Cysteine" },
+            { "UGC","Cysteine" },
+            { "UGG","Tryptophan" },
+            { "UAA","STOP" },
+            { "UAG","STOP" },
+            { "UGA","STOP" }
+
+        };
+
+
+    public static string[] Proteins(string strand)
+    {
+        var list = new List<string>();
+
+        for (int i = 0; i < strand.Length; i += 3)
+        {
+            var protein = dict[strand.Substring(i, 3)];
+            if (protein == "STOP") break;
+            list.Add(protein);
+        }
+
+        return list.ToArray();
+    }
+
+}
