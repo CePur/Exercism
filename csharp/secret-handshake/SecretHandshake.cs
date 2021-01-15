26571ba92bca23d0a100975edbaf0410b519a120
CePur <cenkpurtas@hotmail.com>
2021-01-15 15:40:19 +0300

    Add new exercise

--- csharp/secret-handshake/SecretHandshake.cs
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -1,9 +1,48 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class SecretHandshake
 {
     public static string[] Commands(int commandValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int binary = int.Parse(Convert.ToString(commandValue, 2));
+        var strList = new List<string>();
+        string word = "";
+
+        var reserved = binary > 10000 ? true : false;
+        if (reserved == true) binary = binary - 10000;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (binary % 10 == 1)
+            {
+                switch (i)
+                {
+                    case 0:
+                        word = "wink";
+                        break;
+
+                    case 1:
+                        word = "double blink";
+                        break;
+                    case 2:
+                        word = "close your eyes";
+                        break;
+                    case 3:
+                        word = "jump";
+                        break;
+                }
+
+                strList.Add(word);
+                binary = binary - 1;
+            }
+
+            binary = binary / 10;
+        }
+
+        if (reserved) strList.Reverse();
+        return strList.ToArray();
     }
 }
